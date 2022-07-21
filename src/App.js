a2fea219a4e3181d25663382aa763dc2f565b1af
Marcelo <marcelo011299@gmail.com>
2022-07-21 16:11:11 -0600

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './components/Footer'
+import { Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
 
 function App() {
   return (
-    <div className="App">
-      <h1>GymApp</h1>
-    </div>
+    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
