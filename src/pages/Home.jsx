c82c51b01a9894f5a50c371ab8f4fe05aae4d55f
Marcelo <marcelo011299@gmail.com>
2022-07-23 18:44:11 -0600

    home start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import { Box } from '@mui/material';
 import { useState } from 'react';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
     return (
         <Box>
-            <
+            <HeroBanner />
         </Box>
     )
 }
